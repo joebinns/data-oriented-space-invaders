2a64e403f576d8b8aa573842f533b5566ee29393
Joe Binns <joebinns.95@gmail.com>
2023-10-29 15:38:47 +0100

    Clean-up projectile spawner

--- Data Oriented Space Invaders/Assets/Scripts/ProjectileSpawner/ProjectileSpawnerAuthoring.cs
+++ Data Oriented Space Invaders/Assets/Scripts/ProjectileSpawner/ProjectileSpawnerAuthoring.cs
@@ -15,8 +15,6 @@ class SpawnerBaker : Baker<ProjectileSpawnerAuthoring>
         var entity = GetEntity(TransformUsageFlags.None);
         AddComponent(entity, new ProjectileSpawner
         {
-            // By default, each authoring GameObject turns into an Entity.
-            // Given a GameObject (or authoring component), GetEntity looks up the resulting Entity.
             Prefab = GetEntity(authoring.Prefab, TransformUsageFlags.Dynamic),
             SpawnTransform = GetEntity(authoring.SpawnTransform, TransformUsageFlags.Dynamic),
             NextSpawnTime = 0.0f,
--- Data Oriented Space Invaders/Assets/Scripts/ProjectileSpawner/ProjectileSpawnerSystem.cs
+++ Data Oriented Space Invaders/Assets/Scripts/ProjectileSpawner/ProjectileSpawnerSystem.cs
@@ -1,42 +1,32 @@
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Burst;
-using Unity.Mathematics;
-using Unity.Rendering;
 
+[BurstCompile]
 [UpdateInGroup(typeof(LateSimulationSystemGroup))]
 public partial struct ProjectileSpawnerSystem : ISystem
 {
-    public void OnCreate(ref SystemState state) { }
-
-    public void OnDestroy(ref SystemState state) { }
-
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        // Queries for all Spawner components. Uses RefRW because this system wants
-        // to read from and write to the component. If the system only needed read-only
-        // access, it would use RefRO instead.
         foreach (RefRW<ProjectileSpawner> projectileSpawner in SystemAPI.Query<RefRW<ProjectileSpawner>>())
         {
             ProcessSpawner(ref state, projectileSpawner);
         }
     }
 
+    [BurstCompile]
     private void ProcessSpawner(ref SystemState state, RefRW<ProjectileSpawner> spawner)
     {
-        // If the next spawn time has passed.
-        if (spawner.ValueRO.NextSpawnTime < SystemAPI.Time.ElapsedTime)
-        {
-            Entity projectile = state.EntityManager.Instantiate(spawner.ValueRO.Prefab);
-
-            state.EntityManager.SetComponentData(projectile, LocalTransform.FromPosition
-            (
-                SystemAPI.GetComponent<LocalTransform>(spawner.ValueRO.SpawnTransform).Position
-            ));
-
-            // Resets the next spawn time.
-            spawner.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
-        }
+        if (!(spawner.ValueRO.NextSpawnTime < SystemAPI.Time.ElapsedTime)) return;
+        // Reset the next spawn time
+        spawner.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
+        
+        // Spawn projectile
+        Entity projectile = state.EntityManager.Instantiate(spawner.ValueRO.Prefab);
+        state.EntityManager.SetComponentData(projectile, LocalTransform.FromPosition
+        (
+            SystemAPI.GetComponent<LocalTransform>(spawner.ValueRO.SpawnTransform).Position
+        ));
     }
 }

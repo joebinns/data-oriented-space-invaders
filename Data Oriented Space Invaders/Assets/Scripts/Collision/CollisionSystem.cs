af5d681b48fb78edf8c82de8ee31f189e8c4ee94
joebinns <hello@joebinns.com>
2023-12-02 22:45:59 +0100

    Add player collision

--- Data Oriented Space Invaders/Assets/Scripts/Collision/CollisionSystem.cs
+++ Data Oriented Space Invaders/Assets/Scripts/Collision/CollisionSystem.cs
@@ -10,17 +10,30 @@ public partial struct CollisionSystem : ISystem
 	public void OnUpdate(ref SystemState state)
 	{
 		var entityCommandBuffer = new EntityCommandBuffer(Allocator.Temp);
-		foreach (var invaderAspect in SystemAPI.Query<InvaderAspect>())
+		var squareDistanceThreshold = 0.75f;
+
+		foreach (var projectileAspect in SystemAPI.Query<ProjectileAspect>())
 		{
-			foreach (var projectileAspect in SystemAPI.Query<ProjectileAspect>())
+			foreach (var invaderAspect in SystemAPI.Query<InvaderAspect>())
 			{
 				if (projectileAspect.VerticalVelocity < 0f) continue; // Invader projectiles should not collider with other invaders
 
 				var squareDistance = math.distancesq(invaderAspect.Position, projectileAspect.Position);
-				if (squareDistance <= (0.75f))
+				if (squareDistance <= squareDistanceThreshold)
 				{
 					entityCommandBuffer.DestroyEntity(projectileAspect.Entity);
 					entityCommandBuffer.DestroyEntity(invaderAspect.Entity);
+				}
+			}
+
+			foreach (var playerAspect in SystemAPI.Query<PlayerAspect>())
+			{
+				if (projectileAspect.VerticalVelocity > 0f) continue; // Player projectiles should not collider with the player
+
+				var squareDistance = math.distancesq(playerAspect.Position, projectileAspect.Position);
+				if (squareDistance <= squareDistanceThreshold)
+				{
+					entityCommandBuffer.DestroyEntity(projectileAspect.Entity);
 				}
 			}
 		}
